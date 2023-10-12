36f024e4c74bff125db73b577d2492ece35a2beb
Jyoo380 <124613319+Jyoo380@users.noreply.github.com>
2023-10-12 12:04:18 +0530

    Create whatfruit?.py

---
+++ whatfruit?.py
@@ -0,0 +1,10 @@
+def american():
+    print("68 70 123 112 ")
+def standard():
+    print("49 110 101 95")
+def code_for():
+    print("64 112 112 108")
+def information():
+    print("101 95 102 114")
+def exchange():
+    print("117 105 116 125")
